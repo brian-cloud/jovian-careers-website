c42ce10521bea045aa42bfc6f7ee40d548396ba0
brian-cloud <55059224+brian-cloud@users.noreply.github.com>
2023-12-13 09:56:49 +0000

    Added dynamic data

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
-app = Flask( __name__)
+app = Flask(__name__)
+JOBS = [{
+    'id': 1,
+    'title': 'Python Developer',
+    'location': 'New York',
+    'salary': '$100,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Nairobi',
+    'salary': '$100,000'
+}, {
+    'id': 3,
+    'title': 'Business Analyst',
+    'location': 'New Delhi',
+    'salary': '$100,000'
+}]
+
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
